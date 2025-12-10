ae05a7b05b914c0027333126ea38aeba844ecb40
Youssef <8957457@alu.murciaeduca.es>
2025-12-10 09:33:26 +0100

    auth con breeze

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProyectosController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'getHome'])
+    ->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +20,25 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::prefix('proyectos')->group(function () {
+    Route::get('/', [ProyectosController::class, 'getIndex']);
+
+    
+
+    Route::get('show/{id}', [ProyectosController::class, 'getShow'])->where('id', '[0-9]+');
+
+    
+
+    Route::group(['middleware'=>'auth'], function () {
+        Route::get('create', [ProyectosController::class, 'getCreate']);
+        Route::get('edit/{id}', [ProyectosController::class, 'getEdit'])->where('id', '[0-9]+');
+
+        Route::post('store', [ProyectosController::class, 'store']);
+
+        Route::put('update/{id}', [ProyectosController::class, 'update'])->where('id', '[0-9]+');
+    });
 });
 
 require __DIR__.'/auth.php';
